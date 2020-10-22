f240e6fdd3f574ad38d3081b9708a1360b7f3774
soraya de lisa <soraya.delisa@gmail.com>
2020-10-22 21:44:47 +0200

    initial commit

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,40 @@
+class CocktailsController < ApplicationController
+
+
+  #GET /cocktails
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @doses = @cocktail.doses
+  end
+
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+
+    respond_to do |format|
+      if @cocktail.save
+        format.html { redirect_to @cocktail, notice: 'Cocktail was successfully created.' }
+        format.json { render :show, status: :created, location: @cocktail }
+      else
+        format.html { render :new }
+        format.json { render json: @cocktail.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
+
+end
